bdffaf4b76833f4fb91905aaa7ab62207404e8d1
EmekaAgara <emekaagara@gmail.com>
2024-08-09 20:43:02 +0100

    first section

--- app/page.js
+++ app/page.js
@@ -1,5 +1,44 @@
+import HeroSection from "@/components/HeroSection";
+// import ProductCard from "@/components/ProductCard";
+import Wrapper from "@/components/Wrapper";
 import Image from "next/image";
 
-export default function Home() {
-  return <main className="">homepage</main>;
+export default function Home({ products }) {
+  return (
+    <main className="">
+      homepage
+      <HeroSection />
+      <Wrapper>
+        {/* heading and paragaph start */}
+        <div className="text-center max-w-[800px] mx-auto my-[50px] md:my-[80px]">
+          <div className="text-[28px] md:text-[34px] mb-5 font-semibold leading-tight">
+            What is Lorem Ipsum?
+          </div>
+          <div className="text-md md:text-xl">
+            What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown
+          </div>
+        </div>
+        {/* heading and paragaph end */}
+
+        {/* products grid start */}
+        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {products?.data?.map((product) => (
+            <ProductCard key={product?.id} data={product} />
+          ))} */}
+        {/* <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard />
+                    <ProductCard /> */}
+        {/* </div> */}
+        {/* products grid end */}
+      </Wrapper>
+    </main>
+  );
 }
