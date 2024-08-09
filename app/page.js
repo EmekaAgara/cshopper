b8701dc04ab8d1414b96e1de1a8222228fbe44ff
EmekaAgara <emekaagara@gmail.com>
2024-08-09 21:26:03 +0100

    demo category page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 import HeroSection from "@/components/HeroSection";
-// import ProductCard from "@/components/ProductCard";
+import ProductCard from "@/components/ProductCard";
 import Wrapper from "@/components/Wrapper";
 import Image from "next/image";
 
@@ -23,20 +23,20 @@ export default function Home({ products }) {
         {/* heading and paragaph end */}
 
         {/* products grid start */}
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
-          {products?.data?.map((product) => (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-14 px-5 md:px-0">
+          {/* {products?.data?.map((product) => (
             <ProductCard key={product?.id} data={product} />
           ))} */}
-        {/* <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard /> */}
-        {/* </div> */}
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+          <ProductCard />
+        </div>
         {/* products grid end */}
       </Wrapper>
     </main>
