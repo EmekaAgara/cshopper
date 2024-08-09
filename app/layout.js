0c0659c56b3f714a536451ee66f7d635a7c49ee9
EmekaAgara <emekaagara@gmail.com>
2024-08-09 17:45:51 +0100

    header components

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,16 @@
-import { Inter } from "next/font/google";
+import { Inter, Urbanist } from "next/font/google";
 import "./globals.css";
 import { headers } from "next/headers";
 import { cookieToInitialState } from "wagmi";
 import { config } from "@/config";
 import AppKitProvider from "@/context";
+import Header from "@/componennts/Header";
+import Footer from "@/componennts/Footer";
+import { Provider } from "react-redux";
+import store from "@/store/store";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const urbanist = Urbanist({ subsets: ["latin"] });
 
 export const metadata = {
   title: "CShopper App",
@@ -16,8 +21,12 @@ export default function RootLayout({ children }) {
   const initialState = cookieToInitialState(config, headers().get("cookie"));
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={urbanist.className}>
+        {/* <Provider store={store}> */}
+        <Header />
         <AppKitProvider initialState={initialState}>{children}</AppKitProvider>
+        {/* <Footer /> */}
+        {/* </Provider> */}
       </body>
     </html>
   );
