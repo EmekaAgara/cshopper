d41c3b69b2abd22e1fa503d07b55dd65ba2f1e8d
EmekaAgara <emekaagara@gmail.com>
2024-08-13 15:00:17 +0100

    css fix

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -6,13 +6,19 @@ import { IoMdHeartEmpty } from "react-icons/io";
 import RelatedProducts from "@/components/RelatedProducts";
 import ReactMarkdown from "react-markdown";
 import SendTransaction from "@/components/SendTransaction";
+
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
-  DialogBackdrop,
-  DialogPanel,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
   DialogTitle,
-} from "@headlessui/react";
-import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+  DialogTrigger,
+} from "@/components/ui/dialog";
+// import { Input } from "@/components/ui/input";
+// import { Label } from "@/components/ui/label";
 
 export async function generateStaticParams() {
   return products.map((product) => ({
@@ -26,8 +32,6 @@ export default function ProductDetails({ params }) {
   if (!product) {
     return <div>Product not found</div>;
   }
-
-  const [open, setOpen] = useState(true);
 
   return (
     <div className="w-full md:py-20">
@@ -171,9 +175,46 @@ export default function ProductDetails({ params }) {
         </div>
 
         <RelatedProducts products={products} />
-      </Wrapper>
 
-      {/* Dialog */}
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline">Edit Profile</Button>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when you're done.
+              </DialogDescription>
+            </DialogHeader>
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                {/* <Label htmlFor="name" className="text-right">
+                  Name
+                </Label>
+                <Input
+                  id="name"
+                  defaultValue="Pedro Duarte"
+                  className="col-span-3"
+                /> */}
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                {/* <Label htmlFor="username" className="text-right">
+                  Username
+                </Label>
+                <Input
+                  id="username"
+                  defaultValue="@peduarte"
+                  className="col-span-3"
+                /> */}
+              </div>
+            </div>
+            <DialogFooter>
+              <Button type="submit">Save changes</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </Wrapper>
     </div>
   );
 }
