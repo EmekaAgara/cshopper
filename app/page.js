f13b91c86b78bf32f1ae42e4ff9123243bdd8d40
EmekaAgara <emekaagara@gmail.com>
2024-08-15 14:33:50 +0100

    product card fix

--- app/page.js
+++ app/page.js
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="">
       <HeroSection />
       <Wrapper>
-        <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-5 mb-7 px-5 md:px-0">
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 mb-7 px-5 md:px-0">
           {products.map((product) => (
             <ProductCard key={product.id} data={product} />
           ))}
