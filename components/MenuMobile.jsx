dc253dc6fd551780ca69318266ffd86e3f133c32
EmekaAgara <emekaagara@gmail.com>
2024-08-17 22:31:49 +0100

    header update

--- components/MenuMobile.jsx
+++ components/MenuMobile.jsx
@@ -4,16 +4,16 @@ import { BsChevronDown } from "react-icons/bs";
 
 const data = [
   { id: 1, name: "Home", url: "/" },
-  { id: 2, name: "About", url: "/about" },
-  { id: 3, name: "Categories", subMenu: true },
-  { id: 4, name: "Contact", url: "/contact" },
+  { id: 2, name: "About Us", url: "/" },
+  { id: 3, name: "Products", subMenu: true },
+  { id: 4, name: "Contact Us", url: "/" },
 ];
 
 const subMenuData = [
-  { id: 1, name: "Jordan", doc_count: 11 },
-  { id: 2, name: "Sneakers", doc_count: 8 },
-  { id: 3, name: "Running shoes", doc_count: 64 },
-  { id: 4, name: "Football shoes", doc_count: 107 },
+  { id: 1, name: "Fashion", doc_count: 20 },
+  { id: 2, name: "Real Estate", doc_count: 108 },
+  { id: 3, name: "Gadgets", doc_count: 89 },
+  { id: 4, name: "Others", doc_count: 25 },
 ];
 
 const MenuMobile = ({
